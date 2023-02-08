4829a92e0e14ef0b41a96642e57fefe629f2ac7d
LSJ <sejin376@naver.com>
2023-02-08 16:52:52 +0900

    pc/모바일 수정

--- script/main.js
+++ script/main.js
@@ -5,11 +5,13 @@ const menu = document.querySelector(".menu");
 
 //event 추가 : .addEventListener() vs Event 제거: .removeEventListener
 opener.addEventListener("click", function () { //클릭 : 마우스 클릭 이벤트
-  menu.classList.remove("hide");//숨겨져있던 .menu 나타내기.
+  //menu.classList.remove("hide");//숨겨져있던 .menu 나타내기.
+  menu.style.display = "bolck";
   console.log("hide 클래스를 제거했습니다.");
 });
 
 closer.addEventListener("click", function () { //클릭 : 마우스 클릭 이벤트
-  menu.classList.add("hide"); 
+  //menu.classList.add("hide"); 실제로는 문제 없지만 테스트 환경 고려. 
+  menu.style.display = "none";
   console.log("hide 클래스를 추가했습니다.");
 });
