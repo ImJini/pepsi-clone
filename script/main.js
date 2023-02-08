d3aca7e573eb8c52001799806bc532444f1d2f34
LSJ <sejin376@naver.com>
2023-02-08 17:30:53 +0900

    어바웃 마무리하는중

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ const menu = document.querySelector(".menu");
 //event 추가 : .addEventListener() vs Event 제거: .removeEventListener
 opener.addEventListener("click", function () { //클릭 : 마우스 클릭 이벤트
   //menu.classList.remove("hide");//숨겨져있던 .menu 나타내기.
-  menu.style.display = "bolck";
+  menu.style.display = "block";
   console.log("hide 클래스를 제거했습니다.");
 });
 
